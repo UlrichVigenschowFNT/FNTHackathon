86dad8d9dfa4ae783d29713b23214a7063e24e9b
michael.voegeli <michael.voegeli@fntsoftware.com>
2017-03-07 11:49:44 +0100

    add methods to REST boundary

--- src/main/java/com/fntsoftware/hackathon/devobst/catalog/boundary/FruitResource.java
+++ src/main/java/com/fntsoftware/hackathon/devobst/catalog/boundary/FruitResource.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import com.fntsoftware.hackathon.devobst.catalog.boundary.model.FruitModel;
 
 /**
- * Boundary Service for fruits.
+ * Boundary Service for fruits. (REST)
  */
 @Stateless
 @Path("fruits")
@@ -23,12 +31,57 @@ import com.fntsoftware.hackathon.devobst.catalog.boundary.model.FruitModel;
 @Produces(MediaType.APPLICATION_JSON)
 public class FruitResource {
 	
+	@Context
+	protected UriInfo uriInfo;
+	
 	@GET
 	public Response listFruits(){
-		 List<FruitModel> fruitModels = new ArrayList<>();
+		List<FruitModel> fruitModels = new ArrayList<>();
+		
+		FruitModel fruitModel = new FruitModel();
+		fruitModel.setUuid("dummyId");
+		fruitModel.setName("Apfel");
+		fruitModel.setSpecies("Elstar");		
+		fruitModel.setColor("Rot");
+		fruitModels.add(fruitModel);
+		
+		fruitModel = new FruitModel();
+		fruitModel.setUuid("dummyId2");
+		fruitModel.setName("Apfel");
+		fruitModel.setSpecies("Granny Smith");		
+		fruitModel.setColor("Grün");
+		fruitModels.add(fruitModel);
+		
+		// TODO get fruits from controller
 		
 		return Response.ok(new GenericEntity<List<FruitModel>>(fruitModels) {
 		}).build();
+	}
+	
+	@GET
+	@Path("{uuid}")
+	public Response getFruit(@PathParam("uuid") final String uuid){
+		FruitModel fruitModel = new FruitModel();
+		fruitModel.setUuid(uuid);
+		fruitModel.setName("Apfel");
+		fruitModel.setSpecies("Elstar");		
+		fruitModel.setColor("Rot");
+		
+		// TODO get concrete fruit from controller and remove the dummy
+		
+		return Response.ok(fruitModel).build();
+	}
+	
+	@POST
+	public Response createFruit(FruitModel fruitModel){
+		
+		String fruitUuid = "dummyId";
+		
+		// TODO create the fruit
+		
+		final ResponseBuilder responseBuilder = Response.status(Status.CREATED);
+		responseBuilder.location(javax.ws.rs.core.UriBuilder.fromUri(uriInfo.getBaseUri()).path(FruitResource.class).path("/{id}").build(fruitUuid));
+		return responseBuilder.build();				
 	}
 
 }
--- src/main/java/com/fntsoftware/hackathon/devobst/catalog/boundary/model/FruitModel.java
+++ src/main/java/com/fntsoftware/hackathon/devobst/catalog/boundary/model/FruitModel.java
@@ -6,12 +6,16 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name="")
 public class FruitModel {
 	
+	
+
 	@XmlElement(name="_uuid")
 	private String uuid;
 	
 	private String name;
 	
 	private String color;
+	
+	private String species;
 
 	public String getUuid() {
 		return uuid;
@@ -35,6 +39,14 @@ public class FruitModel {
 
 	public void setColor(String color) {
 		this.color = color;
+	}
+	
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
 	}
 
 }
