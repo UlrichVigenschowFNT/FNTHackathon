18363e227b6084d8cb6795ce94a14ad133948a05
Ulrich Vigenschow <ulrich.vigenschow@fntsoftware.com>
2017-03-31 17:42:38 +0200

    add missing directory with dummy file; fix creating password file

--- docker/app/start-payara-service.sh
+++ docker/app/start-payara-service.sh
@@ -12,17 +12,19 @@ set -e
 
 ./asadmin --port 8080 start-domain
 
-./asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource --restype javax.sql.ConnectionPoolDataSource --property portNumber=${DB_PORT}:password=${DB_PASSWORD}:user=${DB_USER}:serverName=${DB_SERVER_NAME}:databaseName=${DB_NAME} --ping true ${CONNECTION_POOLNAME}
 
 ./asadmin deploy /webapps/*.war
 
 # create temporary password files
-echo 'AS_ADMIN_PASSWORD=\n\AS_ADMIN_NEWPASSWORD='$ADMIN_PASSWORD'\nEOF\n' >> tmpfile
-echo 'AS_ADMIN_PASSWORD='$ADMIN_PASSWORD'\nEOF\n' >> pwdfile
+echo 'AS_ADMIN_PASSWORD=' >> tmpfile
+echo 'AS_ADMIN_NEWPASSWORD='$ADMIN_PASSWORD >> tmpfile
+echo 'AS_ADMIN_PASSWORD='$ADMIN_PASSWORD >> pwdfile
 
-./asadmin -u $ADMIN_USER --passwordfile tmpfile change-admin-password
+./asadmin -u $ADMIN_USER --passwordfile tmpfile --interactive=false change-admin-password
+./asadmin -u $ADMIN_USER --passwordfile pwdfile --interactive=false enable-secure-admin
 
-./asadmin -u $ADMIN_USER --passwordfile pwdfile enable-secure-admin
+# JDBC
+./asadmin -u $ADMIN_USER --passwordfile pwdfile --interactive=false create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource --restype javax.sql.ConnectionPoolDataSource --property portNumber=${DB_PORT}:password=${DB_PASSWORD}:user=${DB_USER}:serverName=${DB_SERVER_NAME}:databaseName=${DB_NAME} --ping true ${CONNECTION_POOLNAME}
 
 # remove password files
 rm tmpfile pwdfile
