d344b0266aa07c2d4594bd30d90fd6925c400cbd
AST <AST@AST-PC>
2017-04-01 13:52:00 +0200

    next step

--- docker/payara-4.1.1-full/start-payara-service.sh
+++ docker/payara-4.1.1-full/start-payara-service.sh
@@ -1,21 +1,33 @@
 ###########################################
 #
-# Start domain via asadmin, change admin 
-# password and enable secure remote
-# admin-console. After script do sleep
+# Start domain via asadmin, change admin
+# password, enable remote admin console and
+# deploy webapplication. After script do sleep
 # in a endless loop to keep service alive.
 #
 ###########################################
 #!/bin/bash
+set -e
+
 ./asadmin --port 8080 start-domain
 
-./asadmin change-admin-password
+./asadmin deploy /webapps/*.war
 
-./asadmin enable-secure-admin
+# create temporary password files
+echo 'AS_ADMIN_PASSWORD=' >> tmpfile
+echo 'AS_ADMIN_NEWPASSWORD='$ADMIN_PASSWORD >> tmpfile
+echo 'AS_ADMIN_PASSWORD='$ADMIN_PASSWORD >> pwdfile
+
+./asadmin -u $ADMIN_USER --passwordfile tmpfile change-admin-password
+
+./asadmin -u $ADMIN_USER --passwordfile pwdfile enable-secure-admin
+
+# remove password files
+rm tmpfile pwdfile
 
 ./asadmin restart-domain
 
 while true
 do
-    sleep 10000
+    sleep 1d
 done
