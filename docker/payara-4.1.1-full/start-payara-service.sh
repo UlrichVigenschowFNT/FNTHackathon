557832152137f193a61d26ce2fc02b8deb1bf6a3
infl4mes <florian.sachsenmaier@gmx.net>
2017-04-01 11:13:30 +0200

    docker setting init

---
+++ docker/payara-4.1.1-full/start-payara-service.sh
@@ -0,0 +1,23 @@
+###########################################
+#
+# Start domain via asadmin, change admin 
+# password and enable secure remote
+# admin-console. After script do sleep
+# in a endless loop to keep service alive.
+#
+###########################################
+#!/bin/bash
+set -e
+
+./asadmin --port 8080 start-domain
+
+./asadmin change-admin-password
+
+./asadmin enable-secure-admin
+
+./asadmin restart-domain
+
+while true
+do
+    sleep 10000
+done
